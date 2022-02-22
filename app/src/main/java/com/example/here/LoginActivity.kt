c6abee8d3f9b1d19d78c38e48ad5a29002c22065
JaeHyeon <jaehyeon0825@gmail.com>
2022-02-22 18:56:25 +0700

    add viewBinding

--- app/src/main/java/com/example/here/LoginActivity.kt
+++ app/src/main/java/com/example/here/LoginActivity.kt
@@ -2,10 +2,26 @@ package com.example.here
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
+import com.example.here.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityLoginBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
